b77e1e1f64a627d918c284edd8300d6b1e59754d
Srinidhi Prasad <srinidhiprasad05@gmail.com>
2020-09-27 12:32:57 +0530

    text1

--- src/modals/App/App.js
+++ src/modals/App/App.js
@@ -4,7 +4,6 @@ import Example from '../Carousel/Carousel';
 import NavBarExample from "../NavBar/NavBar";
 import CardsList from "../Cards/Cards";
 import Component from "../iframe/iframe";
-import Maps from '../Maps/Maps';
 
 class App extends React.Component{
   constructor(props){
@@ -13,7 +12,7 @@ class App extends React.Component{
 
 
     this.state = {
-      cardDetails : [{src: "/maps.png", alt : "1", title : "Maps"}, {src : "/graphs.jpg", alt : "2", title : "Graphs"},  {src : "/beds.jpg", alt : "3", title : "Beds"}],
+      cardDetails : [{src: "./maps.png", alt : "1", title : "Maps"}, {src : "./graphs.jpg", alt : "2", title : "Graphs"},  {src : "./beds.jpg", alt : "3", title : "Beds"}],
       iframe : {src: "https://www.atlistmaps.com/map/198971f6-b520-435d-bdaf-bf83fb739538?share=true", allow :"geolocation" , frameborder:"0", scrolling:"no", width : "100%", height : "400px" }
     }
   }
@@ -26,7 +25,6 @@ class App extends React.Component{
         <Example />
         <CardsList cardDetails = {this.state.cardDetails} />
         <Component iframe = {this.state.iframe}/>
-        <Maps />
       </div>
     )
   }
--- src/modals/Carousel/Carousel.js
+++ src/modals/Carousel/Carousel.js
@@ -14,20 +14,20 @@ const items = [
     id: 1,
     altText: 'Slide 1',
     caption: 'Slide 1',
-    src: "/beds.jpg"
+    src: "./beds.jpg"
 
   },
   {
     id: 2,
     altText: 'Slide 2',
     caption: 'Slide 2',
-    src : "/banner1.jpg"
+    src : "./banner1.jpg"
   },
   {
     id: 3,
     altText: 'Slide 3',
     caption: 'Slide 3',
-    src: "/maps.png"
+    src: "./maps.png"
   }
 ];
 
